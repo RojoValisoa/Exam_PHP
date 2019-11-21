16dc9b7cddc952edef62973d502ba4a453f96f1a
RojoValisoa <rojo.valisoa.andrianantenaina@esti.mg>
2019-11-21 13:59:53 +0300

    front de base

--- index.php
+++ index.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP EXAM</title>
+  <meta charset="UTF-8">
+  <title>Interactive Radial Menu</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+  <link rel="stylesheet" href="assets/style.css">
+
 </head>
 <body>
-    Personne voiture
+<!-- partial:index.partial.html -->
+<div class="toggle" id="toggle" onclick="menu-expand()">
+<i class="fa fa-plus" id="plus"></i>
+  </div>
+<div class="menu" id="menu">
+  <a href="#">
+    <i class="fa fa-microphone"></i>
+  </a>
+   <a href="#">
+    <i class="fa fa-user"></i>
+  </a>
+  <a href="#">
+    <i class="fa fa-video-camera"></i>
+  </a>
+  <a href="#">
+    <i class="fa fa-envelope"></i>
+  </a>
+  <a href="#">
+    <i class="fa fa-camera"></i>
+  </a>
+  <a href="#">
+    <i class="fa fa-bell"></i>
+  </a>
+</div>
+<!-- partial -->
+  <script  src="assets/script.js"></script>
+
 </body>
 </html>
